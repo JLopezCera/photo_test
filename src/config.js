ac8a7c994177c36b545a94db1926b523c4083e1f
Jesus Lopez <Jesus.Lopez@lacity.org>
2019-09-16 14:57:46 -0700

    name change

--- src/config.js
+++ src/config.js
@@ -1,3 +1,15 @@
-Config = {
-    bucket: 'camille'
-};module.exports = Config;
+var fs = require('fs');
+
+var str = `
+    Config = {
+        bucket: '${process.env.COSMIC_BUCKET}'
+    };
+
+    module.exports = Config;
+`;
+fs.writeFile("./src/config.js", str, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
